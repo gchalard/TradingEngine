d313372e1b022d187109b54e76876fd3e3f26cf2
pptozorux <guilhem.chagper@gmail.com>
2026-04-02 14:56:54 +0200

    feat: update the mock test strategy to use broker and handle ticks and EOS separately

--- tests/stategy/mock.py
+++ tests/stategy/mock.py
@@ -1,6 +1,7 @@
 from collections import deque
 from dataclasses import dataclass, field
 
+from src.broker.backtest import Backtest
 from src.datasource.dataframe import DataFrame
 from src.event.event import Event
 from src.core import run
@@ -15,23 +16,40 @@ import numpy as np
 class MockStrategy(Strategy):
     closes: deque[float] = field(default_factory=lambda: deque(maxlen=201))
     close_returns: deque[float] = field(default_factory=lambda: deque(maxlen=200))
-    momentum_200: float =field(init=False)
+    momentum_200: float = field(init=False)
+    broker: Backtest = field(default_factory=Backtest)
+
+    def __post_init__(self) -> None:
+        self.broker.connect()
 
 
     def onEvent(self, event: Event) -> None:
+        if event["type"] == "tick":
+            self.onTick(event)
+        
+        elif event["type"] == "EOS":
+            self.onEOS()
+
+    def onTick(self, event: Event) -> None:
         last_close = self.closes[-1] if len(self.closes) > 0 else event["data"]["Close"]
         self.closes.append(event["data"]["Close"])
         self.close_returns.append(event["data"]["Close"] / last_close)
         self.momentum_200 = np.prod(self.close_returns)
 
         if self.momentum_200 > 1:
-            print("Buy")
+            close = event["data"]["Close"]
+            self.broker.buy_at_market(close, 1.0)
+        elif len(self.broker.current_position) > 0:
+            self.broker.sell_at_market(event["data"]["Close"], 1.0)
         else:
-            print("Hold")
+            pass
 
+    def onEOS(self) -> None:
+        self.broker.stats()
+        self.broker.disconnect()
 
     def next(self) -> None:
-        time.sleep(1)
+        pass
 
     def core(self) -> None:
         pass
