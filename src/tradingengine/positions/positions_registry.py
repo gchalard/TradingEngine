2f146fd19d9dfd3fe0aae68539ee0cb9d27ef5f5
pptozorux <guilhem.chagper@gmail.com>
2026-04-02 16:50:23 +0200

    feat: add position timestamps

--- src/tradingengine/positions/positions_registry.py
+++ src/tradingengine/positions/positions_registry.py
@@ -1,6 +1,8 @@
 from tradingengine.positions.position import Position
 from tradingengine.enums.position_status import PositionStatus
 
+from datetime import datetime
+
 
 class PositionsRegistry(list[Position]):
 
@@ -8,4 +10,10 @@ class PositionsRegistry(list[Position]):
     def gross_equity_curve(self) -> list[float]:
         return [
             (position.gross_pnl) for position in self if position.status == PositionStatus.CLOSED
+        ]
+
+    @property
+    def exit_timestamps(self) -> list[datetime]:
+        return [
+            position.close["timestamp"] for position in self if position.status == PositionStatus.CLOSED
         ]
