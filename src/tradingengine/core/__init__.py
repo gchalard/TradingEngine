8895c1d88b0ed97652ef1db2f5d384b7c4498ab3
pptozorux <guilhem.chagper@gmail.com>
2026-04-02 16:37:08 +0200

    feat: pass kwargs to strategy via run function

--- src/tradingengine/core/__init__.py
+++ src/tradingengine/core/__init__.py
@@ -1,10 +1,10 @@
 from tradingengine.datasource.datasource import DataSource
 from tradingengine.strategy.strategy import Strategy
 
-def run(datasource: DataSource, strategy: Strategy) -> None:
+def run(datasource: DataSource, strategy: Strategy, **kwargs) -> None:
     datasource.connect()
     while not datasource.eos:
         event = datasource.get_event()
         strategy.onEvent(event)
         strategy.next()
-    strategy.onEvent({"type": "EOS"})
+    strategy.onEvent({"type": "EOS", **kwargs})
