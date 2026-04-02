ba5970f5ade5faae525dae13bd2d924d3f4084d7
pptozorux <guilhem.chagper@gmail.com>
2026-04-02 16:27:02 +0200

    feat: create initial plotting with gross equity curve plot

--- src/tradingengine/broker/broker.py
+++ src/tradingengine/broker/broker.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from dataclasses import dataclass, field
+from datetime import datetime
 
 
 from tradingengine.positions.positions_registry import PositionsRegistry
@@ -26,7 +28,43 @@ class Broker(ABC):
         print(f"Current position: {self.current_position}")
         print(f"Gross equity curve: {self.historical_positions.gross_equity_curve}")
 
-    def plot(self) -> None:
-        fig = go.Figure()
+    def plot(self, closes: list[float] = None, timestamps: list[datetime] = None) -> None:
+        fig = make_subplots(rows=1, cols=1, specs=[[{"secondary_y": True}]])
+
+        X = timestamps if timestamps is not None else list(range(len(closes)))
+
+        fig.add_trace(
+            go.Scatter(
+                x=X,
+                y=self.historical_positions.gross_equity_curve,
+                name="Gross equity curve",
+                yaxis="y1",
+            ),
+        )
+
+        fig.update_layout(
+            title="Gross equity curve",
+            xaxis_title="Timestamp" if timestamps is not None else "Index",
+            yaxis={
+                "title": "Gross equity curve",
+            }
+
+        )
+
+        if closes:
+            fig.add_trace(
+                go.Scatter(
+                    x=X,
+                    y=closes,
+                    name="Closes",
+                    yaxis="y2",
+                ),
+            )
+
+            fig.update_yaxes(
+                title="Closes",
+                secondary_y=True,
+            )
+
 
         fig.show()
